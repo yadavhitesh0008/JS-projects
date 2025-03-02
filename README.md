b34b86302fe289e3c59f11680f9f8a88abe137e8
yadavhitesh0008 <yadavhitesh0008@gmail.com>
2025-03-02 20:28:03 +0530

    final commit

---
+++ README.md
@@ -0,0 +1,5 @@
+JS mini projects 
+
+1. stone-paper-scissor game
+2. To- do List
+3. Weather app
---
+++ to do list /script.js
@@ -0,0 +1,45 @@
+const inputBox = document.querySelector("#input-box");
+const listContainer = document.querySelector("#list-container");
+
+const btn =document.querySelector("#add-button");
+
+
+ function addTask() {
+ 
+    if(inputBox.value === ''){
+        alert("you must write something!");
+    } else{
+        let li = document.createElement("li");
+        li.innerHTML =inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
+ }
+
+ 
+ btn.addEventListener("click",addTask);
+
+ listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+
+ },false);
+
+ function saveData(){
+    localStorage.setItem("data" , listContainer.innerHTML);
+ }
+
+ function showTask(){
+    listContainer.innerHTML =localStorage.getItem("data");
+ }
+ showTask();
